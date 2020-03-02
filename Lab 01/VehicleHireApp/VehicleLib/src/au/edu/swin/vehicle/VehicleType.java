058cece4b539f62981a1e85a7b3f8eca973ba262
Jonathan Yong <jonathan.yong@papercut.com>
2020-03-02 18:20:48 +1100

    first commit

---
+++ Lab 01/VehicleHireApp/VehicleLib/src/au/edu/swin/vehicle/VehicleType.java
@@ -0,0 +1,35 @@
+package au.edu.swin.vehicle;
+
+public class VehicleType {
+    private String code;
+    private String description;
+    private Integer seats;
+
+    public VehicleType(String code, String description, Integer seats) {
+        this.code = code;
+        this.description = description;
+        this.seats = seats;
+    }
+
+    public String getCode() {
+        return code.toUpperCase();
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSeats() {
+        return seats;
+    }
+
+}
+
