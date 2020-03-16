2ad81e84ddb2361e557fb9af77874a37047c1d09
Jonathan Yong <jonathan.yong@papercut.com>
2020-03-16 21:53:59 +1100

    lab 4 task

---
+++ Lab 04/ED-JDE-DTO-RI/src/session/MyuserFacadeRemote.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.MyuserDTO;
+import java.util.ArrayList;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author jonathany
+ */
+@Remote
+public interface MyuserFacadeRemote {
+
+    boolean createRecord(MyuserDTO myuserDTO);
+
+    MyuserDTO getRecord(String userid);
+
+    boolean updateRecord(MyuserDTO myuserDTO);
+
+    boolean deleteRecord(String userId);
+
+    ArrayList<MyuserDTO> getRecordsByAddress(String address);
+}
