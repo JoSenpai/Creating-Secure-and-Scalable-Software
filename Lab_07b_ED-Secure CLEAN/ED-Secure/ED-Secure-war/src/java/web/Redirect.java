293d2822e8a32a6cc09b37b5b34867181bc00858
Jonathan Yong <jonathan.yong@papercut.com>
2020-04-27 14:18:43 +1000

    clean lab 7

---
+++ Lab_07b_ED-Secure CLEAN/ED-Secure/ED-Secure-war/src/java/web/Redirect.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import java.io.IOException;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author elau
+ */
+@Named(value = "Redirect")
+@SessionScoped
+public class Redirect extends HttpServlet {
+
+    /**
+     * Creates a new instance of MyLoginManagedBean
+     */
+    public Redirect() {
+    }
+
+    public void mainmenu() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+        if (request.isUserInRole("ED-APP-ADMIN")) {
+            context.getExternalContext().redirect("faces/admin/mainmenu.xhtml");
+        } else if (request.isUserInRole("ED-APP-USERS")) {
+            context.getExternalContext().redirect("faces/user/mainmenu.xhtml");
+        }
+    }
+}
